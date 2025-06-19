eaf4795055a2415b77484d64faec06ffd8fc236e
Shenghao Wang <wangshenghao1993@gmail.com>
2025-06-18 22:43:55 -0400

    Revise logging

--- src/model/classifier.py
+++ src/model/classifier.py
@@ -5,11 +5,13 @@ import torchmetrics
 
 
 class DiabetesRiskClassifier(pl.LightningModule):
-    def __init__(self, model):
+    def __init__(self, model, batch_size: int):
         super(DiabetesRiskClassifier, self).__init__()
+        self.save_hyperparameters()
         self.model = model
         self.loss_fn = nn.BCEWithLogitsLoss()
         self.f1 = torchmetrics.classification.BinaryF1Score()
+        self.batch_size = batch_size
 
     def forward(self, x):
         return self.model(x)
@@ -18,8 +20,22 @@ class DiabetesRiskClassifier(pl.LightningModule):
         x, y = batch
         y_hat = self.forward(x).squeeze()  # Remove extra dimensions
         loss = self.loss_fn(y_hat, y)
-        self.log('train_loss', loss)
-        self.log('train_f1', self.f1(y_hat, y.float()))
+        self.log(
+            'train_loss_epoch',
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            batch_size=self.batch_size,
+        )
+        self.log(
+            'train_f1',
+            self.f1(y_hat, y.float()),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            batch_size=self.batch_size,
+        )
 
         return loss
 
@@ -27,15 +43,29 @@ class DiabetesRiskClassifier(pl.LightningModule):
         x, y = batch
         y_hat = self.forward(x).squeeze() 
         loss = self.loss_fn(y_hat, y)
-        self.log('val_loss', loss)
-        self.log('val_f1', self.f1(y_hat, y.float()))
+        self.log(
+            'val_loss',
+            loss,
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            batch_size=self.batch_size
+        )
+        self.log(
+            'val_f1',
+            self.f1(y_hat, y.float()),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            batch_size=self.batch_size
+        )
     
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self.forward(x).squeeze()
         loss = self.loss_fn(y_hat, y)
         self.log('test_loss', loss)
-        self.log('test_f1', self.f1(y_hat, y.float()))
+        self.log('test_f1', self.f1(y_hat, y.float()), prog_bar=True)
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=8e-4)
