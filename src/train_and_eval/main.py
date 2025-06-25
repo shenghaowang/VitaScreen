186177d4c03307d6a75ad70a6773e9b02c705fa1
Shenghao Wang <wangshenghao1993@gmail.com>
2025-06-24 21:35:23 -0400

    Refactor training script

--- src/train_and_eval/main.py
+++ src/train_and_eval/main.py
@@ -1,56 +1,59 @@
 import time
 from pathlib import Path
 
+import hydra
 import pandas as pd
 import pytorch_lightning as pl
 import torch
 from loguru import logger
+from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from torchinfo import summary
 
-from data.cdc import CDCDataModule
+from data.cdc import IgtdDataModule, NctdDataModule
 from data.transform import nctd_transform
 from model.classifier import DiabetesRiskClassifier
 from model.cnn import ConvNet
+from model.model_type import ModelType
 from train_and_eval.evaluate import compute_metrics
 from train_and_eval.metrics_logger import MetricsLogger
 
 
-def main():
+@hydra.main(version_base=None, config_path="../config", config_name="config")
+def main(cfg: DictConfig):
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+
     torch.manual_seed(seed=42)
 
     # Init data module
-    dm = CDCDataModule(
-        data_file=Path("data/cdcNormalDiabetic.csv"),
-        target_col="Label",
-    )
-    # dm.setup(transform=nctd_transform, downsample=True)
-    dm.setup(transform=nctd_transform)
+    if cfg.model.name == ModelType.NCTD.value:
+        dm = NctdDataModule(
+            data_file=Path(cfg.data.file_path),
+            target_col=cfg.data.target_col,
+        )
+        dm.setup(transform=nctd_transform, downsample=cfg.data.downsample)
+
+    elif cfg.model.name == ModelType.IGTD.value:
+        dm = IgtdDataModule(
+            data_file=Path(cfg.data.file_path),
+            img_dir=Path(cfg.igtd.img_dir),
+            target_col=cfg.data.target_col,
+        )
+        dm.setup()
+
+    else:
+        raise ValueError(f"Unsupported model type: {cfg.model.name}")
 
     # Init CNN model
-    logger.info("Initializing NCTDConvNet model...")
+    logger.info("Initializing CNN model...")
     model = ConvNet()
-    summary(model, input_size=(1, 1, 42, 42))
-
-    # dummy_input = torch.randn(8, 1, 42, 42)
-    # output = model(dummy_input)
-
-    # logger.debug(f"Output shape: {output.shape}")  # should be [8, 2]
-
-    # dataloader = dm.train_dataloader()
-    # batch = next(iter(dataloader))
-    # inputs, labels = batch
-    # logger.debug(f"Input shape: {inputs.shape}")
-    # logger.debug(f"Label shape: {labels.shape}")
-
-    logger.debug(f"MPS available: {torch.backends.mps.is_available()}")
-    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-    logger.debug(f"Current device: {device}")
+    summary(
+        model, input_size=(1, 1, cfg.model.input_dim.nrows, cfg.model.input_dim.ncols)
+    )
 
     logger.info("Training the model...")
     metrics_logger = MetricsLogger()
     early_stop_callback = EarlyStopping(
-        # monitor="val_loss", min_delta=0.001, patience=3, verbose=True, mode="min"
         monitor="val_loss",
         min_delta=0.001,
         patience=5,
@@ -65,15 +68,15 @@ def main():
         verbose=False,
         save_weights_only=False,  # save full model (or True for just weights)
     )
-    classifier = DiabetesRiskClassifier(model=model, batch_size=64, pos_weight=3.0)
+    classifier = DiabetesRiskClassifier(
+        model=model, batch_size=cfg.train.batch_size, pos_weight=cfg.model.pos_weight
+    )
     trainer = pl.Trainer(
-        max_epochs=30,
-        accelerator="mps",
-        devices=1,
+        max_epochs=cfg.train.max_epochs,
+        devices=cfg.train.devices,
         enable_progress_bar=True,
-        log_every_n_steps=50,  # or higher
+        log_every_n_steps=cfg.train.log_every_n_steps,
         enable_model_summary=False,  # optional
-        # callbacks=[metrics_logger]
         callbacks=[metrics_logger, early_stop_callback, checkpoint_callback],
     )
 
@@ -108,17 +111,10 @@ def main():
             y_pred.extend(preds.cpu().numpy())
             y_test.extend(y.cpu().numpy())
 
-    avg_options = ["micro", "macro", "weighted", "binary"]
-
-    results = [compute_metrics(y_test, y_pred, avg) for avg in avg_options]
+    results = [compute_metrics(y_test, y_pred, avg) for avg in cfg.results.avg_options]
     results_df = pd.DataFrame(results)
-    results_df.to_csv("results.csv", index=False)
+    results_df.to_csv(cfg.results.file_path, index=False)
 
-
-# binary,0.8517226426994638,0.6291718170580964,0.12819544138017883,0.21299299089862955
-# binary,0.8511313465783664,0.5647530040053405,0.21307140158670193,0.3094084300996617
-# binary,0.8138796909492274,0.4271156832298137,0.5542123158292407,0.482433543436558
-# binary,0.7998265531378114,0.4038545012587898,0.5858204256390883,0.4781089414182939
 
 if __name__ == "__main__":
     main()
