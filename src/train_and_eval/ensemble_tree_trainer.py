db1ad15dbd086ced08370f4f1d988c3fb460ce2f
Shenghao Wang <wangshenghao1993@gmail.com>
2025-07-02 09:28:26 -0400

    Add docstring

--- src/train_and_eval/ensemble_tree_trainer.py
+++ src/train_and_eval/ensemble_tree_trainer.py
@@ -7,12 +7,27 @@ from train_and_eval.evaluate import compute_metrics
 
 
 class EnsembleTreeTrainer:
+    """
+    Trainer class for ensemble tree-based models, in particular
+    CatBoostClassifier on CDC data.
+    """
+
     def __init__(self, hyperparams: DictConfig):
         hyperparams = OmegaConf.to_container(hyperparams, resolve=True)
         self.model = EnsembleTreeClassifier(**hyperparams)
 
     def setup(self, data_cfg: DictConfig):
-        """Load and prepare the dataset."""
+        """
+        Load and prepare the CDC dataset for training, validation, and testing.
+
+        Parameters
+        ----------
+        data_cfg : DictConfig
+            Hydra/OmegaConf configuration containing:
+                - file_path: Path to the CDC dataset file.
+                - target_col: Name of the target variable column.
+                - downsample: Whether to downsample the data for addressing class imbalance.
+        """
         (
             (X_train, y_train),
             (self.X_val, self.y_val),
@@ -27,13 +42,22 @@ class EnsembleTreeTrainer:
         self.val_pool = Pool(data=self.X_val, label=self.y_val)
 
     def train(self):
-        """Train the model."""
+        """Train the ensemble tree model."""
         self.model.fit(
             self.train_pool, eval_set=self.val_pool, early_stopping_rounds=50
         )
 
     def evaluate(self):
-        """Evaluate the model."""
+        """
+        Evaluate the ensemble tree model on validation and test sets.
+
+        Returns
+        -------
+        y_test : np.ndarray
+            True labels for the test set.
+        y_preds : np.ndarray
+            Predicted labels for the test set.
+        """
         y_preds = self.model.predict(self.X_val)
         val_metrics = compute_metrics(self.y_val, y_preds, avg_option="binary")
 
